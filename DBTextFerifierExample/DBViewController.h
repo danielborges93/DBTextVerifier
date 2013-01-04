2f954b94f68a9cd9e403752577cbcba2c28259f2
Daniel Borges <daniel_borges_93@yahoo.com.br>
2013-01-04 15:06:12 -0300

    asd

---
+++ DBTextFerifierExample/DBViewController.h
@@ -0,0 +1,13 @@
+//
+//  DBViewController.h
+//  DBTextFerifierExample
+//
+//  Created by Daniel Borges on 04/01/13.
+//  Copyright (c) 2013 Daniel Borges Ferreira da SIlva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DBViewController : UIViewController
+
+@end
---
+++ DBTextFerifierExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DBTextFerifierExample
+//
+//  Created by Daniel Borges on 04/01/13.
+//  Copyright (c) 2013 Daniel Borges Ferreira da SIlva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DBAppDelegate class]));
+    }
+}
