69b0d8424bf72b10c0e96522a6aba7d34f2aada6
Daniel Borges <daniel_borges_93@yahoo.com.br>
2013-01-04 14:28:36 -0300

    Created an example project.

---
+++ DBTextVerifier/DBTextVerifier.h
@@ -0,0 +1,13 @@
+//
+//  DBTextVerifier.h
+//  DBTextFerifierExample
+//
+//  Created by Daniel Borges on 04/01/13.
+//  Copyright (c) 2013 Daniel Borges Ferreira da SIlva. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DBTextVerifier : NSObject
+
+@end
---
+++ DBTextVerifier/DBTextVerifier.m
@@ -0,0 +1,13 @@
+//
+//  DBTextVerifier.m
+//  DBTextFerifierExample
+//
+//  Created by Daniel Borges on 04/01/13.
+//  Copyright (c) 2013 Daniel Borges Ferreira da SIlva. All rights reserved.
+//
+
+#import "DBTextVerifier.h"
+
+@implementation DBTextVerifier
+
+@end
