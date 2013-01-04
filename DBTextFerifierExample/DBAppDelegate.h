581dc06ea1c1ad4cae61d454489d1dc4c84fe33f
Daniel Borges <daniel_borges_93@yahoo.com.br>
2013-01-04 14:23:41 -0300

    Initial Commit

---
+++ DBTextFerifierExample/DBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DBAppDelegate.h
+//  DBTextFerifierExample
+//
+//  Created by Daniel Borges on 04/01/13.
+//  Copyright (c) 2013 Daniel Borges Ferreira da SIlva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
